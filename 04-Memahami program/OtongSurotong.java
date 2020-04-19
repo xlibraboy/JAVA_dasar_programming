7fc22fc11ba1f97ef77ea31c5977a90ee8f12d5b
Budi Kurniawan <xlibraboy@gmail.com>
2020-04-19 18:11:26 +0700

    Update OtongSurotong.java

--- 04-Memahami program/OtongSurotong.java
+++ 04-Memahami program/OtongSurotong.java
@@ -2,7 +2,7 @@ public class OtongSurotong {
 
 	public static void main(String[] args){
 
-		System.out.print("Nama ku adalah Otong, teman si ucup");
+		System.out.print("Nama ku adalah Buyung, teman si Upiak");
 
 	}
 }
